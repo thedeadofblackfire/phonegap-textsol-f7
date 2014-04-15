6c3bbbfa40f2d7e982a90ec82d5fd6f7e6d8fa19
Nicolas <nthomas@robotikdose.com>
2014-04-15 15:04:10 +0200

    150414302

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -302,35 +302,8 @@ function createContentPage() {
     );
     return;
 }
-/*
-function createContentPage(){
-	mainView.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link">Back</a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="page2" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="page2.html">Page 2</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
-*/
+
 $$(document).on('click', '.ks-generate-page', createContentPage);
 
 // init textsol
-//app.initialize();
+app.initialize();
